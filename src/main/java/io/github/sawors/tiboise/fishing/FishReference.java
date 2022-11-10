7badb83a59743236a2a998e37c63bab466353f33
Sawors <yt.sawors@gmail.com>
2022-11-10 14:50:32 +0100

    fish variants loading

---
+++ src/main/java/io/github/sawors/tiboise/fishing/FishReference.java
@@ -0,0 +1,8 @@
+package io.github.sawors.tiboise.fishing;
+
+public class FishReference {
+    boolean legendary = false;
+    String name = "unknown fish";
+    double maxweight = 0;
+    double minweight = 0;
+}
--- src/main/java/io/github/sawors/tiboise/fishing/FishingManager.java
+++ src/main/java/io/github/sawors/tiboise/fishing/FishingManager.java
@@ -1,26 +1,84 @@
 package io.github.sawors.tiboise.fishing;
 
+import io.github.sawors.tiboise.Tiboise;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 public class FishingManager implements Listener {
 
-    Map<String, FishItem> fishmap = new HashMap<>();
-    Map<String, LegendaryFishItem> legendaryfishmap = new HashMap<>();
-    Map<String, String> watertypesmap = new HashMap<>();
+    private static Map<String, FishReference> fishmap = new HashMap<>();
+    private static Map<String, FishReference> legendaryfishmap = new HashMap<>();
+    private static Map<String, String> watertypemap = new HashMap<>();
+
 
     public static void loadFishVariants(){
+        File basedir = new File(Tiboise.getPlugin().getDataFolder()+ File.separator+"fishing");
+        File variants = new File(basedir+File.separator+"fish_variants.yml");
+        if(!variants.exists()){
+            try(InputStream in = Tiboise.getPlugin().getResource("fishing/fish_variants.yml"); FileOutputStream out = new FileOutputStream(variants)){
+                if(in != null){
+                    variants.createNewFile();
+                    out.write(in.readAllBytes());
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
 
+        YamlConfiguration data = YamlConfiguration.loadConfiguration(variants);
+        for(String key : data.getKeys(false)){
+            ConfigurationSection section = data.getConfigurationSection(key);
+            if(section != null){
+                String name = key;
+                double maxweight = section.getDouble("max-weight");
+                double minweight = section.getDouble("min-weight");
+                double rarity = section.getDouble("spawn-rarity");
+                List<?> biomes = section.getList("spawn");
+
+                fishmap.put(name, new FishReference());
+
+            }
+        }
     }
 
-    public static void loadLegendaryFishVariants(){
 
+    public static void loadLegendaryFishVariants(){
+        File basedir = new File(Tiboise.getPlugin().getDataFolder()+ File.separator+"fishing");
+        File legendary = new File(basedir+File.separator+"legendary_fish_variants.yml");
+        if(!legendary.exists()){
+            try(InputStream in = Tiboise.getPlugin().getResource("fishing/legendary_fish_variants.yml"); FileOutputStream out = new FileOutputStream(legendary)){
+                if(in != null){
+                    legendary.createNewFile();
+                    out.write(in.readAllBytes());
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void loadWaterZones(){
-
+        File basedir = new File(Tiboise.getPlugin().getDataFolder()+ File.separator+"fishing");
+        File watertypes = new File(basedir+File.separator+"water_types.yml");
+        if(!watertypes.exists()){
+            try(InputStream in = Tiboise.getPlugin().getResource("fishing/water_types.yml"); FileOutputStream out = new FileOutputStream(watertypes)){
+                if(in != null){
+                    watertypes.createNewFile();
+                    out.write(in.readAllBytes());
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
